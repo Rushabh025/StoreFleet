661ea4e8b336754c7240ccd3120a839e921fa168
Rushabh025 <rushabh.mahawarkar@gmail.com>
2025-04-04 17:49:27 +0530

    Placing new order completed.

--- backend/src/order/controllers/order.controller.js
+++ backend/src/order/controllers/order.controller.js
@@ -6,4 +6,26 @@ import { ErrorHandler } from "../../../utils/errorHandler.js";
 
 export const createNewOrder = async (req, res, next) => {
   // Write your code here for placing a new order
+  
+  try {
+    const orderData = req.body;
+
+    if (
+      !orderData.shippingInfo ||
+      !Array.isArray(orderData.orderedItems) || orderData.orderedItems.length === 0 ||
+      !orderData.paymentInfo ||
+      !orderData.itemsPrice ||
+      !orderData.totalPrice ||
+      !orderData.paidAt
+    ) {
+      return next(new ErrorHandler(400, "Missing required order fields"));
+    }
+    orderData.user = req.user._id;
+
+    const orderPlaced = await createNewOrderRepo(orderData);
+    res.status(201).json({ success: true, orderPlaced });
+
+  } catch (error) {
+    return next(new ErrorHandler(500, error));
+  }
 };
